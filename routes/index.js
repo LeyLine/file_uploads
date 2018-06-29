aa9fadeef155e453e45da011fdb62263941b3ade
Adrian Llerena <a.llerena001@gmail.com>
2018-06-28 21:25:46 -0400

    testing image upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const multer = require('multer');
+const Picture = require('../models/picture');
+const upload = multer({
+  dest: './public/uploads/'
+});
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
